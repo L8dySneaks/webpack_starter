c0472fe492078edc96f91df68dc2cd73f82f7307
Steph Symosko <steph.symosko@gmail.com>
2019-02-01 10:12:34 -0600

    update entry point to html file, and get HMR working for html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: path.join(__dirname, 'src'),
+    watchContentBase: true,
+    hot: true
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
   output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
       publicPath: '/'
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'first Test',
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
